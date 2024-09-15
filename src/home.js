5f30e7c8d370478d503b579af025344517a753ed
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-09-14 23:17:58 -0700

    Added content to home module.

--- src/home.js
+++ src/home.js
@@ -1,5 +1,31 @@
 export function renderHome() {
     const contentContainer = document.querySelector("#content");
+    const homeDiv = document.createElement("div");
+    contentContainer.appendChild(homeDiv);
+    homeDiv.classList.add("homeDiv");
 
-    const div = document.createElement("div");
+    // restaurant name
+    const restaurantName = document.createElement("h1");
+    const restaurantNameText = document.createTextNode("Stev's Potatoes");
+    restaurantName.appendChild(restaurantNameText);
+    homeDiv.appendChild(restaurantNameText);
+
+    // restaurant description
+    const description = document.createElement("p");
+    const descriptionText = document.createTextNode("At Stev’s Potatoes, we take pride in offering the finest, premium-quality potatoes in town. As a local upscale restaurant, we specialize in crafting exquisite potato dishes that highlight the rich, earthy flavors of our farm-to-table produce. Every potato we serve is sourced directly from our own local, organic farm, ensuring that every bite is as fresh as it is flavorful. Whether you’re enjoying a classic baked potato or one of our gourmet creations, you’ll taste the care and quality that goes into each dish at Stev’s Potatoes.");
+    description.appendChild(descriptionText);
+    homeDiv.appendChild(description);
+
+    // hours of operation
+    const hoursOfOperation = document.createElement("p");
+    const hoursOfOperationText = document.createTextNode("Monday - Friday: 9:00 AM - 6:00 PM / Saturday & Sunday: 9:00 AM - 10:00 PM");
+    hoursOfOperation.appendChild(hoursOfOperationText);
+    homeDiv.appendChild(hoursOfOperationText);
+
+    // location/address
+    const address = document.createElement("p");
+    const addressText = document.createTextNode("210 Potato Dr, Las Vegas, NV 89118");
+    address.appendChild(addressText);
+    homeDiv.appendChild(addressText);
+
 }
