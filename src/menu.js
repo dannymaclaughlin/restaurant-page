3f03c33038ae3b09e70a495174dd287abb767c53
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-09-28 00:06:23 -0700

    Created 3 divs/sections for three menu items.

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,21 @@
-import "./styles.css";
+import "./styles.css"
 
 export function renderMenu() {
     const contentContainer = document.querySelector("#content");
 
-    const div = document.createElement("div");
+    const menuDiv = document.createElement("div");
+    contentContainer.appendChild(menuDiv);
+    menuDiv.classList.add("menuDiv");
+
+    const item1Div = document.createElement("div");
+    menuDiv.appendChild(item1Div);
+    item1Div.classList.add("item1");
+
+    const item2Div = document.createElement("div");
+    menuDiv.appendChild(item2Div);
+    item2Div.classList.add("item2");
+
+    const item3Div = document.createElement("div");
+    menuDiv.appendChild(item3Div);
+    item3Div.classList.add("item3");
 }
