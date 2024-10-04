14a5a02a8e2fdb2de52f63c4dc457e788d24f63f
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-10-04 12:10:45 -0700

    Assigned a class to the header and navbar, and added padding to both.

--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,7 @@ import "./styles.css"
 export function renderMenu() {
     const contentContainer = document.querySelector("#content");
 
+    // menu main-container
     const menuContainer = document.createElement("div");
     contentContainer.appendChild(menuContainer);
     menuContainer.classList.add("menuContainer");
@@ -18,15 +19,28 @@ export function renderMenu() {
     item1.appendChild(item1Content);
     item1Container.appendChild(item1);
     item1.classList.add("item1");
-    item1Content.classList.add("item1Content");
 
-    // item 2
+    // item 2 container
     const item2Container = document.createElement("div");
     menuContainer.appendChild(item2Container);
     item2Container.classList.add("item2Container");
 
-    // item 3
+    // item 2 content
+    const item2 = document.createElement("p");
+    const item2Content = document.createTextNode("2");
+    item2.appendChild(item2Content);
+    item2Container.appendChild(item2);
+    item2.classList.add("item2");
+
+    // item 3 container
     const item3Container = document.createElement("div");
     menuContainer.appendChild(item3Container);
     item3Container.classList.add("item3Container");
+
+    // item 3 content
+    const item3 = document.createElement("p");
+    const item3Content = document.createTextNode("3");
+    item3.appendChild(item3Content);
+    item3Container.appendChild(item3);
+    item3.classList.add("item3");
 }
