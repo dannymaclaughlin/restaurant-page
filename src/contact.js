0fb6b2a9def742cf8622db0873197a6fcc4aaea7
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-10-04 16:54:31 -0700

    Added header to the contact module with temporary borders for visualization.

--- src/contact.js
+++ src/contact.js
@@ -3,5 +3,17 @@ import "./styles.css";
 export function renderContact() {
     const contentContainer = document.querySelector("#content");
 
-    const div = document.createElement("div");
+    const contactDiv = document.createElement("div");
+    contactDiv.classList.add("contactDiv");
+    contentContainer.appendChild(contactDiv);
+
+    const tabHeaderDiv = document.createElement("div");
+    tabHeaderDiv.classList.add("tabHeaderDiv");
+    contactDiv.appendChild(tabHeaderDiv);
+
+    const tabHeaderElement = document.createElement("h1");
+    tabHeaderDiv.appendChild(tabHeaderElement);
+
+    const tabHeaderText = document.createTextNode("Contact Us");
+    tabHeaderElement.appendChild(tabHeaderText);
 }
