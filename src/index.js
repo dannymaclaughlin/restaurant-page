5e85af32a8671204c937da2765d98c0753a16422
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-10-04 13:21:37 -0700

    Added a click eventlistener to the home, menu, and contact navbar buttons.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,35 @@ import { renderMenu } from "./menu";
 import "./styles.css";
 
 
-// temporarily commenting out this loadPage function to view renderHome page
+// renders page-load.js module
 // loadPage();
 
-// upon pageload, calls on renderHome function to load home.js module
-// renderHome();
-renderMenu();
+// renders home.js module
+renderHome();
+
+// renders menu.js file
+// renderMenu();
+
+// renders contact.js module
+// insert renderContact() here
+
+// tab switching logic ( function to be called after page is loaded )
+function tabSwitchingLogic() {
+    const homeButton = document.querySelector("#home-button");
+    const menuButton = document.querySelector("#menu-button");
+    const contactButton = document.querySelector("#contact-button");
+
+    homeButton.addEventListener('click', () => {
+        alert("you've clicked on the home button")
+    })
+
+    menuButton.addEventListener('click', () => {
+        alert("you've clicked on the menu button")
+    })
+
+    contactButton.addEventListener('click', () => {
+        alert("you've clicked on the contact button")
+    })
+};
+
+tabSwitchingLogic();
