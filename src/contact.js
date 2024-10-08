38043c16169e1c3cd180a7414dfaf67b8f411cae
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-10-07 23:17:52 -0700

    Added content to the Contact Us module.

--- src/contact.js
+++ src/contact.js
@@ -3,17 +3,43 @@ import "./styles.css";
 export function renderContact() {
     const contentContainer = document.querySelector("#content");
 
-    const contactDiv = document.createElement("div");
-    contactDiv.classList.add("contactDiv");
-    contentContainer.appendChild(contactDiv);
+    // main div
+    const contactMainDiv = document.createElement("div");
+    contactMainDiv.classList.add("contactMainDiv");
+    contentContainer.appendChild(contactMainDiv);
 
+    // header div
     const tabHeaderDiv = document.createElement("div");
     tabHeaderDiv.classList.add("tabHeaderDiv");
-    contactDiv.appendChild(tabHeaderDiv);
+    contactMainDiv.appendChild(tabHeaderDiv);
 
+    // header element
     const tabHeaderElement = document.createElement("h1");
     tabHeaderDiv.appendChild(tabHeaderElement);
 
+    // header text
     const tabHeaderText = document.createTextNode("Contact Us");
     tabHeaderElement.appendChild(tabHeaderText);
+
+    // phone number div
+    const contactInfoDiv = document.createElement("div");
+    contactInfoDiv.classList.add("contactInfoDiv");
+    contactMainDiv.appendChild(contactInfoDiv);
+
+    contactInfoDiv.appendChild(document.createTextNode("(702)123-4567"));
+    contactInfoDiv.appendChild(document.createElement('br'));
+    contactInfoDiv.appendChild(document.createTextNode("123 Shoots Dr."))
+    contactInfoDiv.appendChild(document.createElement('br'));
+    contactInfoDiv.appendChild(document.createTextNode("Las Vegas, NV 89118"))
+
+    // phone number element
+    // const contactPhoneNumberElement = document.createElement("p");
+    // contactInfoDiv.appendChild(contactPhoneNumberElement);
+
+    // // phone number text
+    // const contactPhoneNumberText = document.createTextNode("702-123-4567");
+    // contactPhoneNumberElement.appendChild(contactPhoneNumberText);
+    
+    // // contactInfoDiv.appendChild(document.createElement('br'));
+    // contactInfoDiv.appendChild(document.createTextNode("Address"));
 }
