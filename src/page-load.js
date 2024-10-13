0dc04a9fb1f545db382530050686d7ad240232a7
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-10-13 01:23:44 -0700

    Adjusted colors, border radius, and margins.

--- src/page-load.js
+++ src/page-load.js
@@ -10,16 +10,21 @@ export function loadPage() {
     image.src = mainImage;
     contentContainer.appendChild(image);
 
+    // text content div
+    const textContentDiv = document.createElement("div");
+    textContentDiv.classList.add("pageloadTextContentDiv");
+    contentContainer.appendChild(textContentDiv);
+
     // headline
     const headline = document.createElement("h1");
     headline.classList.add("pageloadHeadline");
     const headlineText = document.createTextNode("the best potatoes in town... 🥔");
     headline.appendChild(headlineText);
-    contentContainer.appendChild(headline);
+    textContentDiv.appendChild(headline);
 
     // some text about the restaurant
     const description = document.createElement("p");
     const descriptionText = document.createTextNode("At Stev’s Potatoes, we take pride in offering the finest, premium-quality potatoes in town. As a local upscale restaurant, we specialize in crafting exquisite potato dishes that highlight the rich, earthy flavors of our farm-to-table produce. Every potato we serve is sourced directly from our own local, organic farm, ensuring that every bite is as fresh as it is flavorful. Whether you’re enjoying a classic baked potato or one of our gourmet creations, you’ll taste the care and quality that goes into each dish at Stev’s Potatoes.");
     description.appendChild(descriptionText);
-    contentContainer.appendChild(description);
+    textContentDiv.appendChild(description);
 }
